bf3cce31905afc1d71b21f7d40ce7691a24c48f8
William-95 <verga.william.95@gmail.com>
2022-09-13 21:30:39 +0200

    w

--- WhereDoYouLive/public/js/main.js
+++ WhereDoYouLive/public/js/main.js
@@ -51,19 +51,9 @@ function getCity(inpRicerca) {
       
       .then(result =>  result.categories.forEach((item) => {
 
-        const card = document.createElement("div");
-        card.classList.add('card');
-        const box = document.getElementById('ris');
-        box.style.display = 'flex';
-        box.appendChild(card);
-        card.style.border = '3px solid' + item.color;
-      
-        card.innerHTML =
-          '<br>' +
-          '<h3>' + item.name + '</h3>' +
-          '<progress id="progress" max="10" value="' + item.score_out_of_10 + '"></progress>' +
-          '<h4>' + item.score_out_of_10.toFixed(1) + '/10</h4>'
-    
+       createCard(element='card', className='card' ,parent='ris',elementCard='card');
+       
+
         
            }     
          )               
