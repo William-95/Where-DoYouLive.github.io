5578460745962365027afac91491217679634e62
William-95 <verga.william.95@gmail.com>
2022-09-20 15:35:49 +0200

    Update vite.config.js

--- WhereDoYouLive/vite.config.js
+++ WhereDoYouLive/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite"
-import { resolve } from 'path'
+//import { resolve } from 'path'
 
 export default defineConfig( {
-    entry: resolve(__dirname, 'WhereDoYouLive/index.html'),
-   base:'/WhereDoYouLive/',
+    build: {
+        rollupOptions: {
+          input: {
+            'entry-point-a': path.resolve(__dirname, 'WhereDoYouLive\index.html'),
+            
+          },
+        }
+      },
+   
   
  })
